4a1c495624934dfbf6a2ee883341d93a05a62cec
Martin <laumets.m@gmail.com>
2023-08-23 15:03:58 +0300

    test

--- src/pages/Servers.js
+++ src/pages/Servers.js
@@ -28,6 +28,10 @@ function Servers() {
 
   return (
     <div className="tabbed-content-container">
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      ></meta>
       <div className="tab-buttons">
         {tabs.map((tab) => (
           <TabButton
